140a729b023a4143642ec5fc68153e19dfd6b302
BenjaminBristow <bbristow.uk@gmail.com>
2026-02-21 21:57:50 +0000

    C and C++ tutorial folders

---
+++ C/format specifiers.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+
+int main(){
+    // ******* Format specifiers in print statements *******
+    //Integer
+    int age = 25;
+    printf("%d\n", age);
+    
+    //Float
+    float price = 19.99;
+    printf("%f\n", price);
+    
+    //Long float
+    double pi = 3.1415926538979;
+    printf("%lf\n", pi);
+    
+    //Character
+    char currency = '£';
+    printf("%c\n", currency);
+    
+    //String
+    char name[] = "Benjamin Bristow";
+    printf("%s\n", name);
+    
+    
+    // ******* Optional modifiers *******
+    //Width with spaces before
+    int number = 1;
+    printf("%3d", number);
+    //This sets the minimum length of a string and fills in any gaps beforehand with a space
+    //Examples, output in “blue” and space as ୦
+    
+    int number = 1;
+    printf("%3d", number);
+    //output: “  1”
+    
+    int number = 12;
+    printf("%3d", number);
+    //output: “ 12”
+    
+    int number = 123;
+    printf("%3d", number);
+    //output: “123”
+    
+    int number = 1234;
+    printf("%3d", number);
+    //output: “1234”
+    
+    // Width with spaces after
+    printf("%-3d", number);
+    //This will put all the space after if the minimum number of characters is not met
+    
+    int number = 1;
+    printf("%-3d", number);
+    //output: “1  ”
+    
+    int number = 12;
+    printf("%-3d", number);
+    //output: “12 ”
+    
+    int number = 123;
+    printf("%-3d", number);
+    //output: “123”
+    
+    int number = 1234;
+    printf("%-3d", number);
+    //output: “1234”
+    
+    // Width with 0’s instead of spaces
+    printf("%03d", number);
+    //This will put all the space after if the minimum number of characters is not met    
+    int number = 1;
+    printf("%03d", number);
+    //output: “001”
+    
+    int number = 12;
+    printf("%03d", number);
+    //output: “012”
+    
+    int number = 123;
+    printf("%03d", number);
+    //output: “123”
+    
+    int number = 1234;
+    printf("%03d", number);
+    //output: “1234”
+    
+    
+    // Showing the signs with a + (flags)
+    printf("%+d", number);
+    //This will put all the sign of the number in front
+
+    int number = 123;
+    printf("%+d", number);
+    //output: “+123”
+    
+    int number = -123;
+    printf("%+d", number);
+    //output: “-123”
+    
+    // Precision of decimals
+    float price = 19.99;
+    printf("%.1f", price);
+    //This only shows one decimal point
+    //This can also be used with long floats (%lf)
+    
+    float price = 19.99;
+    printf("%f", price);
+    //output: “19.990000” as a float stores 6 digits after the decimal
+    
+    float price = 19.99;
+    printf("%.2f", price);
+    //output: “19.99”
+    
+    float price = 19.99;
+    printf("%.1f", price);
+    //output: “20.0” as it rounded the unused digit
+    
+    // Combining them all
+    float price1 = 19.99;
+    float price2 = 1.50;
+    float  price3 = -100.00;
+    
+    printf("%+7.2f \n", price1);
+    printf("%+7.2f \n", price2);
+    printf("%+7.2f \n", price3);
+    
+    //output:
+    //“0+19.99”
+    //“00+1.50”
+    //“-100.00”
+
+}
+
+
---
+++ C/main stuff.c
@@ -0,0 +1,46 @@
+// ******* Header files - preprocessor directive *******
+//Includes standard input outputs
+#include <stdio.h> 
+
+//Includes standard boolean
+#include <stdbool.h>
+
+//Includes useful functions for strings and useful for the fgets function
+#include <string.h>
+
+//Includes useful math functions (in “arithmetic operators”)
+#include <math.h>
+
+//Adds functions including Sleep(milliseconds) (for windows) or sleep(seconds) (for linux or mac)
+//#include <windows.h> (for windows) #include <unistd.h> (for linux/mac)
+
+//Needed for random 
+#include <stdlib.h>, #include <time.h>
+
+// ******* Main function *******
+int main ()
+{
+	// your code here
+
+	return 0; //the main function expects a returned integer and it serves an an exit code, 0 
+//means it ran successfully. Non 0 return integer usually means an error
+}
+
+
+/* ******* Semi Colon ******
+- A semi colon must be used after any line of code that doesn't start a block of code
+- Example: if statements, functions, main functions, for loop, while loop are all starting blocks of code and do not need a ;
+- Whereas printf statements, initialising variables and calculations all need a ; at the end of every line to tell the compiler that the line is finished
+*/
+
+// ******* Comments *******
+//Comments are used to explain code and are ignored by the compiler. They are useful for making
+//code easier to understand for other programmers and for yourself when you come back to the code after a long time.
+
+// this is a single line comment
+
+/*
+This is a multi line comment
+It can span multiple lines
+*/ 
+
